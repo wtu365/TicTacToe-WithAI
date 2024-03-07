91c0eef6c4fe66533c9e3c58c931bcf935201931
wtu365 <130800254+wtu365@users.noreply.github.com>
2024-03-06 20:33:27 -0800

    Update tictactoe2players.py with AI search tree

--- tictactoe2players.py
+++ tictactoe2players.py
@@ -13,6 +13,54 @@ def check_win(board):
         return board[1][1]
     return 0
 
+def getAction(board: np.ndarray, actions: list):
+    move = None
+    min_reward = float('inf')
+    for i in range(len(actions)):
+        action = actions.pop(i)
+        print(action)
+        board[action[0]][action[1]] = -1
+        if check_win(board) == -1:
+            return action
+        if (reward := get_max(board.copy(), actions.copy())) < min_reward:
+            move = action
+            min_reward = reward
+        print(reward)
+        board[action[0]][action[1]] = 0
+        actions.insert(i, action)
+    return move
+            
+def get_max(board: np.ndarray, actions: list):
+    if actions == []:
+        return 0
+    max_reward = float('-inf')
+    for i in range(len(actions)):
+        action = actions.pop(i)
+        board[action[0]][action[1]] = 1
+        if check_win(board) == 1:
+            return 1
+        if (reward := get_min(board.copy(), actions.copy())) > max_reward:
+            max_reward = reward
+        board[action[0]][action[1]] = 0
+        actions.insert(i, action)
+    return max_reward
+
+
+def get_min(board: np.ndarray, actions: list):
+    if actions == []:
+        return 0
+    min_reward = float('inf')
+    for i in range(len(actions)):
+        action = actions.pop(i)
+        board[action[0]][action[1]] = -1
+        if check_win(board) == -1:
+            return -1
+        if (reward := get_max(board.copy(), actions.copy())) < min_reward:
+            min_reward = reward
+        board[action[0]][action[1]] = 0
+        actions.insert(i, action)
+    return min_reward 
+
 def print_board(board):
     for i in range(3):
         for j in range(3):
@@ -28,6 +76,40 @@ def print_board(board):
             print("\n-----")
         else:
             print()
+
+def start_gamewithAI():
+    board = np.zeros((3, 3))
+    turn = 0
+    win = 0
+    actions = [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)]
+
+    while (win == 0 and turn < 9):
+        if (turn % 2 == 0):
+            row, column = input("Your turn! Choose row and column to place X in!\n").split()
+            row, column = int(row), int(column)
+            while ((row, column) not in actions):
+                row, column = input("Invalid Input! Please enter a valid row and column.\n").split()
+                row, column = int(row), int(column)
+            board[row][column] = 1
+            print_board(board)
+            actions.remove((row, column))
+            win = check_win(board)
+        elif (turn % 2 == 1):
+            print("It's the AI's turn! \n")
+            action = getAction(board.copy(), actions.copy())
+            board[action[0]][action[1]] = -1
+            print_board(board)
+            actions.remove((action[0], action[1]))
+            win = check_win(board)
+        turn += 1
+    
+    if (win == 1):
+        print("You win!")
+    elif (win == -1):
+        print("The AI wins!")
+    else:
+        print("It's a tie!")
+
 
 def start_game2players():
     board = np.zeros((3, 3))
@@ -67,6 +149,6 @@ def start_game2players():
 if __name__ == "__main__":
     play_flag = input("Would you like to play a game of Tic-Tac-Toe? Enter y for yes and anything else for no.\n")
     while (play_flag == "Y" or play_flag == "y"):
-        start_game2players()
+        start_gamewithAI()
         play_flag = input("Do you want to play again? Enter y for yes and anything else for no.\n")
     print("Thanks for playing!")
